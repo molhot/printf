e4d1fd9981908b2d7e0e10620b283118bbea123e
molhot <sakata19991214@gmail.com>
2022-11-05 18:13:43 +0900

    ncxsdlkj

--- ft_putnbr_string.c
+++ ft_putnbr_string.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:44:34 by satushi           #+#    #+#             */
-/*   Updated: 2022/11/05 18:00:17 by satushi          ###   ########.fr       */
+/*   Updated: 2022/11/05 18:13:18 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,42 @@
 
 size_t inputnbr(long long subject, char* src)
 {
-	long long subject_dig;
 	size_t len;
 
-	subject_dig = 1;
 	len = 0;
-	while(subject_dig <= subject)
-		subject_dig = subject_dig * 10;
-	subject_dig = subject_dig / 10;
 	if(subject < 0)
 	{
-		subject = subject * (-1);
 		len = len + ft_putchar('-');
+		subject = subject * (-1);
 	}
-	while(subject_dig != 0)
+	if(subject < 10)
+		len = len + ft_putchar(src[subject]);
+    else
 	{
-		len = len + ft_putchar(src[subject / subject_dig]);
-		subject = subject % subject_dig;
-		subject_dig = subject_dig / 10;
+		len = len + ft_putaddr_to_hexia(subject / 10, src);
+		len = len + ft_putaddr_to_hexia(subject % 10, src);
 	}
-	return len;
+    return(len);
 }
 
 size_t    ft_putnum_to_hexia(unsigned long num, char *basestring)
 {
-    int max_divited_counter;
-    char *addr_box;
-	size_t len;
+    size_t len;
 
-    max_divited_counter = ft_divide_max(num, 16);
-    addr_box = (char *)malloc(sizeof(char) * (max_divited_counter + 1));
-    addr_box[max_divited_counter - 1] = '\0';
-    max_divited_counter--;
-    while(max_divited_counter != 0)
-    {
-        addr_box[max_divited_counter] = basestring[num % 16];
-        num = num / 16;
-        max_divited_counter--;
-    }
-    addr_box[max_divited_counter] = basestring[num];
 	len = 0;
-	len = len + ft_putchar_string(addr_box);
-	free(addr_box);
-    return (len);
+	if(num < 0)
+	{
+		len = len + ft_putchar('-');
+		num = num * (-1);
+	}
+	if(num < 16)
+		len = len + ft_putchar(basestring[num]);
+    else
+	{
+		len = len + ft_putaddr_to_hexia(num / 16, basestring);
+		len = len + ft_putaddr_to_hexia(num % 16, basestring);
+	}
+    return(len);
 }
 
 size_t    ft_putaddr_to_hexia(long long num, char *basestring)
--- ft_putstr.c
+++ ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: satushi <sakata19991214@gmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 07:44:52 by satushi           #+#    #+#             */
-/*   Updated: 2022/11/05 17:27:08 by satushi          ###   ########.fr       */
+/*   Updated: 2022/11/05 18:07:18 by satushi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,22 +22,12 @@ size_t ft_putchar_string(char *sub_string)
 	size_t len;
 
 	if(sub_string == NULL)
-		return 0;
+	{
+		ft_putchar(NULL);
+		return 6;
+	}
 	len = 0;
 	while(sub_string[len] != '\0')
 		len = len + 1;
 	return (write(1, sub_string, len));
-}
-
-size_t ft_divide_max(long long num, int divide_num)
-{
-    int counter;
-
-    counter = 0;
-    while(num != 0)
-    {
-        num = num / divide_num;
-        counter = counter + 1;
-    }
-    return (counter);
 }
